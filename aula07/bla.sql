d2f46d68bc0feedf3fcb7ea4e13c411fb8874e0a
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2024-07-09 02:17:51 -0300

    feat: :sparkles: Exercícios feitos

--- aula07/bla.sql
+++ aula07/bla.sql
@@ -1,8 +1,11 @@
 -- 40. Consultar o total vendido do Produto 4
 
-select count(pedidos.prd_codigo) from itens_pedidos as pedidos where 
-    prd_codigo = 4;
+select sum(pedidos.itp_qtd) as Total_Vendido from
+    itens_pedidos as pedidos where
+        prd_codigo = 4;
 
 -- 41. Consultar o valor total de cada pedido.
 
-select ped_codigo, pedidos.itp_qtd * pedidos.itp_valor from itens_pedidos as pedidos;
+select ped_numero, sum(pedidos.itp_qtd * pedidos.itp_valor) as
+    Valor_Total from itens_pedidos as pedidos
+        group by ped_numero;
